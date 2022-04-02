18affa5cb18b12ad4afc6ba4929cfe50a791803c
Arnaud BEAUGEON <arnaud.beaugeon@live.fr>
2022-04-02 17:11:16 +0200

    [+] Add HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 20, color: "blue" }}>
+        Hello, this is my home :) !
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
