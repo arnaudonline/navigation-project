dbe9956c5556a7fc90b296a5c20869f987874013
Arnaud BEAUGEON <arnaud.beaugeon@live.fr>
2022-04-03 14:42:12 +0200

    [+] Add Profile in header

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import BurgerMenu from './src/navigation/BurgerMenu';
-import BottomTabs from './src/navigation/BottomTabs';
+import BurgerMenu from "./src/navigation/BurgerMenu";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator  screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='Burger Menu' component={BurgerMenu} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Burger Menu" component={BurgerMenu} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/navigation/BurgerMenu.tsx
+++ src/navigation/BurgerMenu.tsx
@@ -1,5 +1,6 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
+import { Button } from "react-native";
 
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -7,12 +8,22 @@ import BottomTabs from "./BottomTabs";
 
 const Drawer = createDrawerNavigator();
 
-const BurgerMenu = () => {
+const BurgerMenu = ({ navigation }) => {
   return (
     <Drawer.Navigator initialRouteName="Home with BottomTabs">
-      <Drawer.Screen name='Home with BottomTabs' component={BottomTabs} />
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="My profile" component={ProfileScreen} />
+      <Drawer.Screen
+        name="Home with BottomTabs"
+        component={BottomTabs}
+        options={{
+          headerRight: () => (
+            <Button
+              onPress={() => navigation.navigate("Profile")}
+              title="Profile"
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
     </Drawer.Navigator>
   );
 };
